a446ba8a69d3cbd48d19bfa6c5cdbf4508e16dac
Lucas <lucas.chuong.debort@wojo.com>
2026-06-22 15:27:03 +0200

    chore: add font & media

--- app/page.tsx
+++ app/page.tsx
@@ -2,12 +2,13 @@ import AcmeLogo from '@/app/ui/acme-logo';
 import { ArrowRightIcon } from '@heroicons/react/24/outline';
 import Link from 'next/link';
 import styles from '@/app/ui/home.module.css';
+import Image from 'next/image';
 
 export default function Page() {
   return (
     <main className="flex min-h-screen flex-col p-6">
       <div className="flex h-20 shrink-0 items-end rounded-lg bg-blue-500 p-4 md:h-52">
-        {/* <AcmeLogo /> */}
+         <AcmeLogo /> 
       </div>
 
       <div className="mt-4 flex grow flex-col gap-4 md:flex-row">
@@ -26,11 +27,24 @@ export default function Page() {
             <span>Log in</span> <ArrowRightIcon className="w-5 md:w-6" />
           </Link>
         </div>
-        <div
+{/*         <div
           className={styles.shape}
-        />
+        /> */}
         <div className="flex items-center justify-center p-6 md:w-3/5 md:px-28 md:py-12">
-          {/* Add Hero Images Here */}
+          <Image
+            src="/hero-desktop.png"
+            alt="Screenshot"
+            className="hidden md:block"
+            width={1000}
+            height={760}
+          />
+          <Image 
+            src="/hero-mobile.png"
+            alt="Screenshot mobile bro"
+            className="block md:hidden"
+            width={560}
+            height={620}
+          />
         </div>
       </div>
     </main>
---
+++ app/ui/fonts.ts
@@ -0,0 +1,14 @@
+import { Inter } from 'next/font/google';
+import { Lusitana } from 'next/font/google';
+
+export const inter = Inter({
+  subsets: ['latin'],
+  display: 'swap',
+});
+
+export const lusitana = Lusitana({
+  subsets: ['latin'],
+  display: 'swap',
+  weight: ['400', '700'],
+});
+
